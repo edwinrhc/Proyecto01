31ce321d1eea1770c15973917a41aa34edbc3520
Edwin HC <edwinrhc@gmail.com>
2024-06-11 16:39:08 -0500

    first commit

---
+++ src/test/java/org/erhc/app/Proyecto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erhc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
